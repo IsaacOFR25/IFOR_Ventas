1ac61fc919c9617be7e83c73b0edf613088b352a
IsaacOFR25 <20030045@itesa.edu.mx>
2024-02-17 23:08:14 -0600

    Creación de los métodos Post, Get

---
+++ Sales.Api/Controllers/CountriesController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Sales.Api.Controllers;
+using Sales.Api.Data;
+using Sales.Shared.Entites;
+
+namespace Sales.Api.Controllers
+{
+
+    [ApiController]
+    [Route("api/countries")]
+    public class CountriesController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public CountriesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult>PostAsync(Country country) 
+        {
+            _context.Add(country);
+            await _context.SaveChangesAsync();
+            return Ok(country);
+                
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAsync()
+        {
+            return Ok(await _context.Countries.ToListAsync());
+        }
+
+
+
+
+    }//Fin de la clase 
+}
